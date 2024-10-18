ac443fbabf1d1152ea46fc789fcb20b5863d634d
Sanashd <sanashd20@gmail.com>
2024-10-18 15:20:32 +0500

    Add project files.

---
+++ Razorpagesproject/Pages/People.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Razorpagesproject.Pages
+{
+    public class PeopleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+    }
+}
